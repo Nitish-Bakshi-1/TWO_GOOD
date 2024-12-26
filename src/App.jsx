c2745eaec6c1ef96c4ae573a328aa9b526a6b698
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-26 23:13:25 +0530

    cursor

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import HomePage from "./pages/HomePage.jsx";
 import Navbar from "./components/Navbar.jsx";
 import BuyGoodsPage from "./pages/BuyGoodsPage.jsx";
 
+import LocomotiveScroll from "locomotive-scroll";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const locomotiveScroll = new LocomotiveScroll();
+  }, []);
   return (
     <div>
       <Navbar />
