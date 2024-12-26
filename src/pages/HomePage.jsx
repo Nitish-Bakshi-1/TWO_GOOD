e45d7586e4d0b0352a24f122c9487abd9978a517
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-26 20:28:53 +0530

    cursor

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -55,10 +55,10 @@ const HomePage = () => {
     );
     gsap.fromTo(
       videoContainerRef.current,
-      { y: 100, opacity: 0 },
+      { scale: 0.9, opacity: 0 },
       {
-        y: 0,
-        duration: 0.9,
+        scale: 1,
+        duration: 0.5,
         opacity: 1,
         ease: "power3.out",
         delay: 1.3,
