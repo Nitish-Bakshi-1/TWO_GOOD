63ef18c0b3e5aac9f368a6a00383aa7a4b208adb
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-26 21:24:05 +0530

    cursor

--- src/components/Element.jsx
+++ src/components/Element.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Element = () => {
-  return <div className="h-[100%] w-[30%] bg-red-400">Element</div>;
+const Element = ({ imgLink }) => {
+  return (
+    <div className="h-[100%] w-[31%] bg-red-400 overflow-hidden">
+      <img src={imgLink} alt="" />
+    </div>
+  );
 };
 
 export default Element;
--- src/pages/BuyGoodsPage.jsx
+++ src/pages/BuyGoodsPage.jsx
@@ -4,9 +4,9 @@ import Element from "../components/Element";
 const BuyGoodsPage = () => {
   return (
     <div className="relative flex py-[3vw] px-[1vw] justify-between h-[100vh] w-[100%]">
-      <Element />
-      <Element />
-      <Element />
+      <Element imgLink="https://cdn.sanity.io/images/w8f1ak3c/production/3639fc290272fe62d269e8750eff907b2664fc77-3000x5000.png/1.png?rect=1,0,2999,5000&w=640&h=1067&auto=format" />
+      <Element imgLink="https://cdn.sanity.io/images/w8f1ak3c/production/9b0a45cc68406b9288b970064c64511ce937d911-5000x5000.png/Website%20Products%20-%20Alemais%202024.png?rect=1001,0,2999,5000&w=640&h=1067&fit=min&auto=format" />
+      <Element imgLink="https://cdn.sanity.io/images/w8f1ak3c/production/06dac0cbccbec66e8c76afc0cc323e5241cc508b-3000x5000.png/Website%20Slider%20Slice%20Images%20(18).png?rect=1,0,2999,5000&w=640&h=1067&auto=format" />
     </div>
   );
 };
